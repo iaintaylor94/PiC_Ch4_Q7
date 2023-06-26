30abc14c12d545cce9ef7602977d7c2b023efe84
Iain Taylor <iain.taylor.94@gmail.com>
2023-06-26 13:06:44 +0000

    completed program

--- .ccls-cache/@home@runner@Ch4Q7/main.c
+++ .ccls-cache/@home@runner@Ch4Q7/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+  double first = 3.31e-8;
+  double second = 2.01e-7;
+  double third = 7.16e-6;
+  double fourth = 2.01e-8;
+
+  double result = (first * second) / (third + fourth);
+
+  printf ("(%e * %e) / (%e + %e) = %e", first, second, third, fourth, result);
+  
   return 0;
 }
